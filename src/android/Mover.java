2b561c266d762c4f03fbe8fb4b0e0c0e24b77f8d
Adam Duren <adamduren@gmail.com>
2015-08-08 13:49:06 -0400

    Create a JS interface for SFTP connections

--- src/android/Mover.java
+++ src/android/Mover.java
@@ -1,6 +1,10 @@
 package com.alto.mover;
 
-import java.io.OutputStream;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -8,21 +12,47 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.jcraft.jsch.*;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.UUID;
 
 
 public class Mover extends CordovaPlugin {
+    HashMap<String, ChannelSftp> channels = new HashMap();
+
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("testConnection")) {
+        if (action.equals("testConnection") || action.equals("connect")) {
             JSONObject hostConfig = args.getJSONObject(0);
             String user = hostConfig.getString("user");
             String password = hostConfig.getString("password");
             String host = hostConfig.getString("host");
             int port = hostConfig.optInt("port", 22);
-            this.testConnection(user, password, host, port, callbackContext);
+
+            if (action.equals("testConnection")) {
+                this.testConnection(user, password, host, port, callbackContext);
+            } else {
+                this.connect(user, password, host, port, callbackContext);
+            }
             return true;
+        } else if (action.equals("disconnect")) {
+            this.disconnect(args.getString(0), callbackContext);
+            return true;
+        } else if (action.equals("cd")) {
+            this.cd(args.getString(0), args.getString(1), callbackContext);
+            return true;
+        } else if (action.equals("pwd")) {
+            this.pwd(args.getString(0), callbackContext);
+            return true;
+        } else if (action.equals("put")) {
+            this.put(args.getString(0), args.getString(1), args.getString(2), args.getBoolean(3), callbackContext);
+        } else if (action.equals("mkdir")) {
+            this.mkdir(args.getString(0), args.getString(1), args.getBoolean(2), callbackContext);
+        }  else if (action.equals("stat")) {
+            this.stat(args.getString(0), args.getString(1), callbackContext);
         }
+
         return false;
     }
 
@@ -44,11 +74,187 @@ public class Mover extends CordovaPlugin {
             output.close();
 
             channel.rm(testFilename);
-            channel.exit();
+            channel.disconnect();
+
+            session.disconnect();
 
             callbackContext.success();
         } catch (Exception e) {
             callbackContext.error(e.getMessage());
         }
     }
+
+    private void connect(String user, String password, String host, int port, CallbackContext callbackContext) {
+        try {
+            JSch jsch = new JSch();
+
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
+            channel.connect();
+
+            String key = UUID.randomUUID().toString();
+            channels.put(key, channel);
+
+            callbackContext.success(key);
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void disconnect(String channelId, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+        Session session = null;
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+            session = channel.getSession();
+        } catch (JSchException e){
+
+        } finally {
+            channel.disconnect();
+
+            if (session != null) {
+                session.disconnect();
+            }
+        }
+
+        callbackContext.success();
+    }
+
+    private void cd(String channelId, String path, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+            channel.cd(path);
+            callbackContext.success(channel.pwd());
+        } catch (SftpException e){
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void pwd(String channelId, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+            callbackContext.success(channel.pwd());
+        } catch (SftpException e){
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void put(String channelId, String name, String data, boolean ensurePath, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+            if (ensurePath) {
+                _ensurePath(channelId, name, true);
+            }
+
+            OutputStream output = channel.put(name);
+            output.write(data.getBytes());
+            output.close();
+            callbackContext.success(channel.pwd());
+        } catch (Exception e){
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void mkdir(String channelId, String path, boolean recursive, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+
+            if (recursive) {
+                _ensurePath(channelId, path);
+            } else {
+                channel.mkdir(path);
+            }
+
+            callbackContext.success();
+        } catch (Exception e){
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void stat(String channelId, String path, CallbackContext callbackContext){
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            callbackContext.error("Invalid channelId");
+            return;
+        }
+
+        try {
+            channel.stat(path);
+            callbackContext.success();
+        } catch (Exception e){
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void _ensurePath(String channelId, String path) throws Exception {
+        _ensurePath(channelId, path, false);
+    }
+
+    private void _ensurePath(String channelId, String path, boolean excludeLast) throws Exception {
+        ChannelSftp channel = channels.get(channelId);
+
+        if (channel == null) {
+            throw new Exception("Invalid channelId");
+        }
+
+
+        String buildPath = "";
+        if (path.substring(0, 1).equals("/")) {
+            path = path.substring(1);
+            buildPath += "/";
+        }
+
+        String[] pathSegments = path.split("/");
+        int size = pathSegments.length;
+
+        if (excludeLast) {
+            size -= 1;
+        }
+
+        for (int i=0; i<size; i++) {
+            buildPath += pathSegments[i] + "/";
+
+            try {
+                channel.stat(buildPath);
+            } catch (Exception e) {
+                channel.mkdir(buildPath);
+            }
+        }
+    }
+
+
 }
--- www/mover.js
+++ www/mover.js
@@ -1,24 +1,113 @@
-/*global cordova, module*/
+/*global cordova, module, Promise*/
+
+"use strict";
 
 var className = 'Mover';
 
+function promiseHelper(method, params) {
+  params = params || [];
+
+  var promise = new Promise(function(resolve, reject) {
+      cordova.exec(
+        function() { resolve.apply(null, arguments); },
+        function() { reject.apply(null, arguments); },
+        className,
+        method,
+        params
+      );
+    }
+  );
+
+  return promise;
+}
+
+function connectionFactory(user, password, host, port) {
+  var con = new Connection();
+  return con.connect(user, password, host, port);
+}
+
+function Connection() {}
+
+Connection.prototype.connect = function connect(user, password, host, port) {
+  var promise =  promiseHelper(
+    'connect',
+    [{
+      user: user,
+      password: password,
+      host: host,
+      port: port,
+    }]
+  );
+  var _this = this;
+
+  promise.then(function(key) {
+    _this.key = key;
+  });
+
+  return promise;
+};
+
+Connection.prototype.disconnect = function disconnect() {
+  var promise = promiseHelper('disconnect', [this.key]);
+  var _this = this;
+
+  promise.then(function() {
+    _this.key = null;
+  });
+
+  return promise;
+};
+
+Connection.prototype.cd = function cd(path) {
+  var promise = promiseHelper('cd', [this.key, path]);
+  var _this;
+
+  promise.then(function(cwd) {
+    _this.cwd = cwd;
+  });
+
+  return promise;
+};
+
+Connection.prototype.pwd = function pwd() {
+  if (this.cwd) {
+    return Promise.resolve(this.cwd);
+  }
+
+  return promiseHelper('pwd', [this.key]);
+};
+
+Connection.prototype.put = function put(name, data, ensurePath) {
+  ensurePath = !!ensurePath;
+
+  if (typeof data === 'object') {
+    data = JSON.stringify(data);
+  }
+  return promiseHelper('put', [this.key, name, data, ensurePath]);
+};
+
+Connection.prototype.stat = function put(path) {
+  return promiseHelper('stat', [this.key, path]);
+};
+
+Connection.prototype.mkdir = function mkdir(path, recursive) {
+  recursive = !!recursive;
+  return promiseHelper('mkdir', [this.key, path, recursive]);
+};
+
 module.exports = {
   testConnection: function (user, password, host, port) {
-    var promise = new Promise(function(resolve, reject) {
-      cordova.exec(
-        function() { resolve(); },
-        function(errorMessage) { reject(errorMessage); },
-        className,
-        'testConnection',
-        [{
-          user: user,
-          password: password,
-          host: host,
-          port: port,
-        }]
-      );
-    });
+    return promiseHelper(
+      'testConnection',
+      [{
+        user: user,
+        password: password,
+        host: host,
+        port: port,
+      }]
+    );
+  },
+  Connection: Connection,
+  connectionFactory: connectionFactory,
 
-    return promise;
-  }
 };
